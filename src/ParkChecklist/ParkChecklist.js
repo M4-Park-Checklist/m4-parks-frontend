827b95c1a244d132b3ed3c848b954789e5e45672
Joseph Stewart <JosephStewart1752@gmail.com>
2024-02-03 13:50:42 -0500

    Mild restructure of SelectedStateParks props

--- src/ParkChecklist/ParkChecklist.js
+++ src/ParkChecklist/ParkChecklist.js
@@ -52,55 +52,60 @@ const ParkChecklist = ({ apiLink }) => {
   const indexOfFirstPark = indexOfLastPark - parksPerPage;
   const currentParks = parks.slice(indexOfFirstPark, indexOfLastPark);
 
-  return (
-    <div
-      className="park-checklist-container"
-      style={{ backgroundImage: `url(${page})` }}
-    >
-      <h1>National Park Service Service</h1>
-      <div className="below-header-box">
-        <p className="welcome-message">Welcome User!</p>
-        <div className="state-selector">
-          <select onChange={handleStateChange} value={selectedState}>
-            <option value="">Select a State</option>
-            <option value="AL">Alabama</option>
-          </select>
-          <button onClick={handleGoClick}>Go!</button>
+return (
+  <div
+    className="park-checklist-container"
+    style={{ backgroundImage: `url(${page})` }}
+  >
+    <h1>National Park Service Service</h1>
+    {showResults ? (
+      <SelectedStateParks parks={parks} selectedState={selectedState} />
+    ) : (
+      <>
+        <div className="below-header-box">
+          <p className="welcome-message">Welcome User!</p>
+          <div className="state-selector">
+            <select onChange={handleStateChange} value={selectedState}>
+              <option value="">Select a State</option>
+              <option value="AL">Alabama</option>
+            </select>
+            <button onClick={handleGoClick}>Go!</button>
+          </div>
         </div>
-      </div>
-      <h2>{`${totalChecked}/${parks.length} parks visited`}</h2>
-      <h2>Parks Checklist</h2>
-      <ul className="checkbox-list">
-        {currentParks.map((park) => (
-          <li key={park.id}>
-            <input
-              type="checkbox"
-              id={park.id}
-              checked={checkedItems.includes(park.id)}
-              onChange={() => handleCheckboxChange(park.id)}
-            />
-            <label htmlFor={park.id}>{park.fullName}</label>
-          </li>
-        ))}
-      </ul>
-      <div className="pagination">
-        <button onClick={() => handlePageChange(1)}>{"<<"}</button>
-        <button onClick={() => handlePageChange(currentPage - 1)}>{"<"}</button>
-        <span>{`${currentPage}/${Math.ceil(
-          parks.length / parksPerPage
-        )}`}</span>
-        <button onClick={() => handlePageChange(currentPage + 1)}>{">"}</button>
-        <button
-          onClick={() =>
-            handlePageChange(Math.ceil(parks.length / parksPerPage))
-          }
-        >
-          {">>"}
-        </button>
-      </div>
-        {showResults && parks && (<SelectedStateParks parks={parks} selectedState={selectedState}/>)}
-    </div>
-  );
+        <h2>{`${totalChecked}/${parks.length} parks visited`}</h2>
+        <h2>Parks Checklist</h2>
+        <ul className="checkbox-list">
+          {currentParks.map((park) => (
+            <li key={park.id}>
+              <input
+                type="checkbox"
+                id={park.id}
+                checked={checkedItems.includes(park.id)}
+                onChange={() => handleCheckboxChange(park.id)}
+              />
+              <label htmlFor={park.id}>{park.fullName}</label>
+            </li>
+          ))}
+        </ul>
+        <div className="pagination">
+          <button onClick={() => handlePageChange(1)}>{"<<"}</button>
+          <button onClick={() => handlePageChange(currentPage - 1)}>{"<"}</button>
+          <span>{`${currentPage}/${Math.ceil(
+            parks.length / parksPerPage
+          )}`}</span>
+          <button onClick={() => handlePageChange(currentPage + 1)}>{">"}</button>
+          <button
+            onClick={() =>
+              handlePageChange(Math.ceil(parks.length / parksPerPage))
+            }
+          >
+            {">>"}
+          </button>
+        </div>
+      </>
+    )}
+  </div>
+);
 };
 
 export default ParkChecklist;
