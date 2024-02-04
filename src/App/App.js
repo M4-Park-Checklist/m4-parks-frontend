457ecf46fb4d9a3def6c28f9b9c001ac58b0c500
Joseph Stewart <JosephStewart1752@gmail.com>
2024-02-03 22:05:26 -0500

    Wrap router in return

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ParkChecklist from '../ParkChecklist/ParkChecklist';
+import { BrowserRouter as Router } from 'react-router-dom';
 import "./App.css"
 
 const App = () => {
   const apiLink = 'https://developer.nps.gov/api/v1/parks?api_key=jR5uonh1B6R19iLhdweq7nHBs0uGgKgwzqn3BNfb';
 
   return (
-    <div>
+    <Router>
       <ParkChecklist apiLink={apiLink} />
-    </div>
+    </Router>
   );
 };
 
