7de63ede57f8b5126a28018a5a31ad2849859355
Alexandre Hastings <alex.hastings.1227@gmail.com>
2024-02-04 11:57:44 -0500

    fix merge conflict

--- src/ParkChecklist/ParkChecklist.js
+++ src/ParkChecklist/ParkChecklist.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useRoutes, useNavigate } from "react-router-dom";
 import axios from "axios";
+import PropTypes from 'prop-types';
 import "./ParkChecklist.css";
 import page from "../resources/page.png";
 import SelectedStateParks from "../SelectedStateParks/SelectedStateParks";
@@ -10,6 +12,7 @@ const ParkChecklist = ({ apiLink }) => {
   const [selectedState, setSelectedState] = useState("");
   const [showResults, setShowResults] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const navigate = useNavigate();
   const parksPerPage = 50;
 
   useEffect(() => {
@@ -41,6 +44,7 @@ const ParkChecklist = ({ apiLink }) => {
 
   const handleGoClick = () => {
     setShowResults(true);
+    navigate(`/selected-state/${selectedState}`)
   };
 
   const handlePageChange = (newPage) => {
@@ -52,60 +56,82 @@ const ParkChecklist = ({ apiLink }) => {
   const indexOfFirstPark = indexOfLastPark - parksPerPage;
   const currentParks = parks.slice(indexOfFirstPark, indexOfLastPark);
 
-return (
-  <div
-    className="park-checklist-container"
-    style={{ backgroundImage: `url(${page})` }}
-  >
-    <h1>National Park Service Service</h1>
-    {showResults ? (
-      <SelectedStateParks parks={parks} selectedState={selectedState} />
-    ) : (
-      <>
-        <div className="below-header-box">
-          <p className="welcome-message">Welcome User!</p>
-          <div className="state-selector">
-            <select onChange={handleStateChange} value={selectedState}>
-              <option value="">Select a State</option>
-              <option value="AL">Alabama</option>
-            </select>
-            <button onClick={handleGoClick}>Go!</button>
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: (
+        <>
+          <div className="below-header-box">
+            <p className="welcome-message">Welcome User!</p>
+            <div className="state-selector">
+              <select onChange={handleStateChange} value={selectedState}>
+                <option value="">Select a State</option>
+                <option value="AL">Alabama</option>
+              </select>
+              <button onClick={handleGoClick}>Go!</button>
+            </div>
           </div>
-        </div>
-        <h2>{`${totalChecked}/${parks.length} parks visited`}</h2>
-        <h2>Parks Checklist</h2>
-        <ul className="checkbox-list">
-          {currentParks.map((park) => (
-            <li key={park.id}>
-              <input
-                type="checkbox"
-                id={park.id}
-                checked={checkedItems.includes(park.id)}
-                onChange={() => handleCheckboxChange(park.id)}
-              />
-              <label htmlFor={park.id}>{park.fullName}</label>
-            </li>
-          ))}
-        </ul>
-        <div className="pagination">
-          <button onClick={() => handlePageChange(1)}>{"<<"}</button>
-          <button onClick={() => handlePageChange(currentPage - 1)}>{"<"}</button>
-          <span>{`${currentPage}/${Math.ceil(
-            parks.length / parksPerPage
-          )}`}</span>
-          <button onClick={() => handlePageChange(currentPage + 1)}>{">"}</button>
-          <button
-            onClick={() =>
-              handlePageChange(Math.ceil(parks.length / parksPerPage))
-            }
-          >
-            {">>"}
-          </button>
-        </div>
-      </>
-    )}
-  </div>
-);
+          <h2>{`${totalChecked}/${parks.length} parks visited`}</h2>
+          <h2>Parks Checklist</h2>
+          <ul className="checkbox-list">
+            {currentParks.map((park) => (
+              <li key={park.id}>
+                <input
+                  type="checkbox"
+                  id={park.id}
+                  checked={checkedItems.includes(park.id)}
+                  onChange={() => handleCheckboxChange(park.id)}
+                />
+                <label htmlFor={park.id}>{park.fullName}</label>
+              </li>
+            ))}
+          </ul>
+          <div className="pagination">
+            <button onClick={() => handlePageChange(1)}>{"<<"}</button>
+            <button onClick={() => handlePageChange(currentPage - 1)}>
+              {"<"}
+            </button>
+            <span>{`${currentPage}/${Math.ceil(
+              parks.length / parksPerPage
+            )}`}</span>
+            <button onClick={() => handlePageChange(currentPage + 1)}>
+              {">"}
+            </button>
+            <button
+              onClick={() =>
+                handlePageChange(Math.ceil(parks.length / parksPerPage))
+              }
+            >
+              {">>"}
+            </button>
+          </div>
+        </>
+      ),
+    },
+    {
+      path: `/selected-state/${selectedState}`,
+      element: <SelectedStateParks parks={parks} selectedState={selectedState} />,
+    },
+  ]);
+
+  return (
+    <div className="park-checklist-container" style={{ backgroundImage: `url(${page})` }}>
+      <h1>National Park Service Service</h1>
+      {routes}
+    </div>
+  );
 };
 
+
 export default ParkChecklist;
+
+
+ParkChecklist.propTypes = {
+    apiLink: PropTypes.string.isRequired,
+  };
+
+
+
+
+
+
