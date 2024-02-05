3c47d1e3f428c4783bf54395da69096bbffca5fa
Joseph Stewart <JosephStewart1752@gmail.com>
2024-02-05 16:58:18 -0500

    Added containers for styling

--- src/ParkDetails/ParkDetails.js
+++ src/ParkDetails/ParkDetails.js
@@ -12,14 +12,16 @@ function ParkDetails({ foundPark }) {
       <div className="park-details-title">
         <h2 className="single-title">{foundPark.fullName}</h2>
       </div>
-      <div className="park-details-activities">
-        <h3 className="single-activities">Activities</h3>
-        {/* <p>{foundPark.activities}</p> */}
-      </div>
-      <div className="park-details-weather">
-        <h3 className="single-weather">Weather</h3>
-        <p>{foundPark.weatherInfo}</p>
-      </div>
+      <section className="park-info">
+        <div className="park-details-activities">
+          <h3 className="single-activities">Activities</h3>
+          {/* <p>{foundPark.activities}</p> */}
+        </div>
+        <div className="park-details-weather">
+          <h3 className="single-weather">Weather</h3>
+          <p>{foundPark.weatherInfo}</p>
+        </div>
+      </section>
     </div>
   );
 }
