dc3a17dc539f952ab2354f292a28f04723ea8c82
Joseph Stewart <JosephStewart1752@gmail.com>
2024-02-08 17:09:44 -0500

    Changed object keys changed to show values

--- src/ParkDetails/ParkDetails.js
+++ src/ParkDetails/ParkDetails.js
@@ -6,27 +6,40 @@ function ParkDetails({ foundPark }) {
   if (!foundPark) {
     return null;
   }
-
+  console.log(foundPark);
   return (
     <div className="park-details-container">
       <div className="park-details-title">
-        <h2 className="single-title">{foundPark.fullName}</h2>
+        <h2 className="single-title">{foundPark.attributes.name}</h2>
       </div>
       <section className="image-container">
         <img
-         className="single-image"
-         src={foundPark.images[1].url}
-         alt={foundPark.images[0].altText}
+          className="single-image"
+          src={foundPark.attributes.media[1].url}
+          alt={foundPark.attributes.media[0].altText}
         />
       </section>
       <section className="park-info">
         <div className="park-details-activities">
-          <h3 className="single-activities">Activities</h3>
-          {/* <p>{foundPark.activities}</p> */}
+          <h1 className="single-activities">Activities</h1>
+          <p>1. {foundPark.attributes.things_to_do[0]}</p>
+          <p>2. {foundPark.attributes.things_to_do[1]}</p>
+          <p>3. {foundPark.attributes.things_to_do[2]}</p>
+          <p>4. {foundPark.attributes.things_to_do[3]}</p>
+          <p>5. {foundPark.attributes.things_to_do[4]}</p>
         </div>
         <div className="park-details-weather">
-          <h3 className="single-weather">Weather</h3>
-          <p>{foundPark.weatherInfo}</p>
+          <h1 className="single-weather">Weather</h1>
+          <h3>Current Weather</h3>
+          <p>{foundPark.attributes.current_weather.condition}</p>
+          <p>
+            Temperature: {foundPark.attributes.current_weather.temperature} °F
+          </p>
+          <p>
+            Feels Like: {foundPark.attributes.current_weather.feels_like} °F
+          </p>
+          <p>Humidity: {foundPark.attributes.current_weather.humidity}%</p>
+          {/* <img>{foundPark.attributes.current_weather.icon}</img> */}
         </div>
       </section>
     </div>
@@ -35,16 +48,15 @@ function ParkDetails({ foundPark }) {
 
 export default ParkDetails;
 
-
 ParkDetails.propTypes = {
   foundPark: PropTypes.shape({
     fullName: PropTypes.string.isRequired,
     images: PropTypes.arrayOf(
       PropTypes.shape({
         url: PropTypes.string.isRequired,
-        altText: PropTypes.string.isRequired
+        altText: PropTypes.string.isRequired,
       })
     ).isRequired,
-    weatherInfo: PropTypes.string.isRequired
-  }).isRequired
+    weatherInfo: PropTypes.string.isRequired,
+  }).isRequired,
 };
