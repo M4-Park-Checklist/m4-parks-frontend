2e3c36a32ae13b4cdc5fdf9a29f97ada3f41106e
Alexandre Hastings <alex.hastings.1227@gmail.com>
2024-02-29 17:52:40 -0500

    iamge carosoul working

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,25 +1,19 @@
 import { Carousel } from "@material-tailwind/react";
 import React from "react";
- 
 export default function NewCarousel({ parkMedia }) {
-  console.log(parkMedia)
-  let images = parkMedia.map(image => {
-      <img
-        className="h-full w-full object-cover"
-        src={image.url}
-        alt={image.altText}
-      />
-   })
-   console.log(images)
+  console.log(parkMedia);
+  let images = parkMedia.map((image, index) => (
+    <img
+      key={index} // Don't forget to add a unique key for each image in a list
+      className="h-full w-full object-cover"
+      src={image.url}
+      alt={image.altText}
+    />
+  ));
+  console.log(images);
   return (
     <Carousel className="rounded-xl">
-      <img 
-        className="h-full w-full object-cover"
-        src={parkMedia[1].url}
-        alt={parkMedia[1].altText}
-      />
-
-
+      {images}
     </Carousel>
   );
 }
