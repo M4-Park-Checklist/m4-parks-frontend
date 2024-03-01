3ca063922a34220cc859f620eec0f20089e1781f
Joseph Stewart <JosephStewart1752@gmail.com>
2024-02-29 21:43:57 -0500

    Changed image size and arrow distance

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,19 +1,23 @@
 import { Carousel } from "@material-tailwind/react";
 import React from "react";
+import './Carousel.css'
+
 export default function NewCarousel({ parkMedia }) {
   console.log(parkMedia);
   let images = parkMedia.map((image, index) => (
     <img
       key={index} // Don't forget to add a unique key for each image in a list
-      className="h-full w-full object-cover"
+      className="carousel-image"
       src={image.url}
       alt={image.altText}
     />
   ));
   console.log(images);
   return (
-    <Carousel className="rounded-xl">
-      {images}
-    </Carousel>
+    <div className="carousel-container"> 
+      <Carousel className="rounded-xl">
+        {images}
+      </Carousel>
+    </div>
   );
 }
